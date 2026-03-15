15c3c4433b28ce8c71c00cc1b0e6514989bfaa77
phani <vsphanikumar777@gmail.com>
2026-03-15 10:53:47 +0530

    terraform

--- 00-vpc/parameters.tf
+++ 00-vpc/parameters.tf
@@ -3,3 +3,21 @@
   type  = "String"
   value = module.vpc.vpc_id
 }   
+
+ resource "aws_ssm_parameter" "public_subnet_ids" {                # SSM Parameter Store is used to store the output values of the VPC module as parameters that can be accessed by other modules or applications. This allows for better organization and management of infrastructure resources.
+  name  = "/${var.project}/${var.environment}/public_subnet_ids"
+  type  = "StringList"
+  value = join(",", module.vpc.public_subnet_ids)  # Convert the list of public subnet IDs to a comma-separated string for storage in SSM Parameter Store.
+}
+
+ resource "aws_ssm_parameter" "private_subnet_ids" {
+  name  = "/${var.project}/${var.environment}/private_subnet_ids"
+  type  = "StringList"
+  value = join(",", module.vpc.private_subnet_ids)
+}
+
+ resource "aws_ssm_parameter" "database_subnet_ids" {
+  name  = "/${var.project}/${var.environment}/database_subnet_ids"
+  type  = "StringList"
+  value = join(",", module.vpc.database_subnet_ids)
+}   
