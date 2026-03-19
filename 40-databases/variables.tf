79b97ec8d51f932f6642ac3870e3280b4f833bfb
phani <vsphanikumar777@gmail.com>
2026-03-19 13:12:14 +0530

    terraform infra

--- 40-databases/variables.tf
+++ 40-databases/variables.tf
@@ -9,3 +9,15 @@ variable "environment" {
     default = "dev"
   
 }
+
+variable "zone_id" {
+
+    default = "Z104354227Z66ZI6A1KBL"
+  
+}
+
+variable "domain_name" {
+
+    default = "devopsskillup.online"
+  
+}
