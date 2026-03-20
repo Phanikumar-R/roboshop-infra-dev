d582821e2a5249bb5fdd7126bf695e9ea64e526f
phani <vsphanikumar777@gmail.com>
2026-03-20 10:32:40 +0530

    terraform infra

--- 20-sg-rules/main.tf
+++ 20-sg-rules/main.tf
@@ -58,5 +58,13 @@ resource "aws_security_group_rule" "mysql_bastion" {
   source_security_group_id = local.bastion_sg_id
   security_group_id = local.mysql_sg_id
 }
-
+resource "aws_security_group_rule" "rabbitmq_bastion" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  # which segment of the network can access this rule, here we are allowing only bastion to access mongodb on port 22
+  source_security_group_id = local.bastion_sg_id
+  security_group_id = local.rabbitmq_sg_id
+}
 
