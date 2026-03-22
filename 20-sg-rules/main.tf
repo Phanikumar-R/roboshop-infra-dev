55c6fbb1bacf917857b4259b1e0c85a186ff3cdf
phani <vsphanikumar777@gmail.com>
2026-03-22 11:16:55 +0530

    terraform infra

--- 20-sg-rules/main.tf
+++ 20-sg-rules/main.tf
@@ -80,4 +80,23 @@ resource "aws_security_group_rule" "backend_alb_bastion" {
   source_security_group_id = local.bastion_sg_id
   security_group_id = local.backend_alb_sg_id
 }
+resource "aws_security_group_rule" "catalogue_bastion" {   
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  # which segment of the network can access this rule, here we are allowing only bastion to access mongodb on port 22
+  source_security_group_id = local.bastion_sg_id
+  security_group_id = local.catalogue_sg_id
+}
+
+resource "aws_security_group_rule" "catalogue_backend_alb" {   
+  type              = "ingress"
+  from_port         = 8080
+  to_port           = 8080
+  protocol          = "tcp"
+  # which segment of the network can access this rule, here we are allowing only bastion to access mongodb on port 22
+  source_security_group_id = local.backend_alb_sg_id
+  security_group_id = local.catalogue_sg_id
+}
 
