e758a91a297f360096f5ff925e5735a292c11ad1
phani <vsphanikumar777@gmail.com>
2026-03-23 22:36:07 +0530

    terraform infra

--- 60-catalogue/main.tf
+++ 60-catalogue/main.tf
@@ -50,3 +50,128 @@ resource "aws_ami_from_instance" "catalogue" {
   source_instance_id = aws_instance.catalogue.id
   depends_on = [ aws_ec2_instance_state.catalogue ]
 }
+
+# Create target group
+
+resource "aws_lb_target_group" "catalogue" {
+  name     = "${var.project}-${var.environment}-catalogue"
+  port     = 8080
+  protocol = "HTTP"
+  vpc_id   = local.vpc_id
+  deregistration_delay = 60
+  health_check {
+    healthy_threshold = 2
+    interval = 10
+    matcher = "200-299"
+    path= "/health"
+    port = 8080
+    protocol = "HTTP"
+    timeout = 2
+    unhealthy_threshold = 3
+
+  }
+}
+
+
+# Create launch template
+
+resource "aws_launch_template" "catalogue" {
+  name = "${var.project}-${var.environment}-catalogue"
+
+  
+  image_id = aws_ami_from_instance.catalogue.id
+
+
+# Once autoscaling sees the less traffic it will terminate the instances
+  instance_initiated_shutdown_behavior = "terminate"
+
+  
+  instance_type = "t3.micro"
+
+  vpc_security_group_ids = [local.catalogue_sg_id]
+
+  # each time we apply terraform this version will be updated as default
+  update_default_version = true
+  # tags for instances created by launch template through auto scaling
+  tag_specifications {
+    resource_type = "instance"
+
+    tags =  merge (
+          {
+
+
+          Name = "${var.project}-${var.environment}-catalogue"
+        },
+          local.common_tags
+    )
+  }
+
+  # Tags for volumes created by instances
+
+  tag_specifications {
+    resource_type = "volume"
+
+    tags =  merge (
+          {
+
+
+          Name = "${var.project}-${var.environment}-catalogue"
+        },
+          local.common_tags
+    )
+  }
+# tags for launch template
+  tags = merge (
+
+    {
+
+      Name = "${var.project}-${var.environment}-catalogue"
+    },
+
+    local.common_tags
+  )
+
+  
+}
+
+
+
+# Creating auto scaling
+
+/* resource "aws_autoscaling_group" "catalogue" {
+  name                      = "${var.project}-${var.environment}-catalogue"
+  max_size                  = 10
+  min_size                  = 1
+  health_check_grace_period = 120
+  health_check_type         = "ELB"
+  desired_capacity          = 1
+  force_delete              = false
+  
+  launch_template {
+    id      = aws_launch_template.catalogue.id
+    version = "$Latest"
+  }
+
+  
+  vpc_zone_identifier       = [local.private_subnet_id]
+  target_group_arns = [aws_lb_target_group.catalogue.arn]
+
+ 
+  tag {
+    key                 = "Name"
+    value               = "${var.project}-${var.environment}-catalogue"
+    propagate_at_launch = true
+  }
+
+  timeouts {
+    delete = "15m"
+  }
+
+  tag {
+    key                 = "lorem"
+    value               = "ipsum"
+    propagate_at_launch = false
+  }
+} */
+
+
