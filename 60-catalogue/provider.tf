bb09e11599e8a3a8efe5ae460b9160b784c32a95
phani <vsphanikumar777@gmail.com>
2026-03-28 19:11:24 +0530

    terraform infra

--- 60-catalogue/provider.tf
+++ 60-catalogue/provider.tf
@@ -8,7 +8,7 @@ terraform {
 
    backend "s3" {
     bucket = "remote-state-daws-88s-dev"  
-    key    = "roboshop-dev-catalogue"    # here make sure give unique name for each module
+    key    = "roboshop-dev-catalogue-info"    # here make sure give unique name for each module
     region = "us-east-1"
     encrypt = true
     use_lockfile = true 
