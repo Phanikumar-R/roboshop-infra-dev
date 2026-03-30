e9484ce7f744b46e25da2508a14f9d74051d3796
phani <vsphanikumar777@gmail.com>
2026-03-30 22:48:20 +0530

    terraform infra

--- 98-openvpn/variables.tf
+++ 98-openvpn/variables.tf
@@ -8,13 +8,3 @@ variable "environment" {
     default = "dev"
   
 }
-variable "zone_id" {
-
-    default = "Z104354227Z66ZI6A1KBL"
-  
-}
-
-variable "domain_name" {
-
-    default = "devopsskillup.online"
-}
