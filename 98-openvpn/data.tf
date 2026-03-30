3b9e63fe8ee1c2a54952cdd1e86bacb28f325dd4
phani <vsphanikumar777@gmail.com>
2026-03-30 23:27:19 +0530

    terraform infra

--- 98-openvpn/data.tf
+++ 98-openvpn/data.tf
@@ -1,11 +1,15 @@
-# Step 1: Fetch the latest Free Tier Ubuntu AMI
-data "aws_ami" "ubuntu" {
-  most_recent = true
-  owners      = ["099720109477"] # Canonical
+data "aws_ami" "openvpn" {
+  most_recent      = true
+  owners           = ["679593333241"]
 
   filter {
     name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+    values = ["OpenVPN Access Server Community Image-8fbe3379-*"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
   }
 
   filter {
@@ -14,32 +18,10 @@ data "aws_ami" "ubuntu" {
   }
 }
 
-# Step 2: Read subnet and security group from SSM (your existing setup)
 data "aws_ssm_parameter" "public_subnet_ids" {
-  name = "/${var.project}/${var.environment}/public_subnet_ids"
+    name = "/${var.project}/${var.environment}/public_subnet_ids"
 }
 
 data "aws_ssm_parameter" "openvpn_sg_id" {
-  name = "/${var.project}/${var.environment}/openvpn_sg_id"
-}
-
-# Step 3: Launch EC2 instance and install OpenVPN via user_data
-resource "aws_instance" "openvpn" {
-  ami                    = data.aws_ami.ubuntu.id
-  instance_type          = "t2.micro" # Free Tier eligible
-  subnet_id              = element(split(",", data.aws_ssm_parameter.public_subnet_ids.value), 0)
-  vpc_security_group_ids = [data.aws_ssm_parameter.openvpn_sg_id.value]
-
-  user_data = <<-EOF
-              #!/bin/bash
-              apt-get update -y
-              apt-get install -y openvpn
-              # Optional: enable OpenVPN service on boot
-              systemctl enable openvpn
-              systemctl start openvpn
-              EOF
-
-  tags = {
-    Name = "roboshop-dev-openvpn"
-  }
+    name = "/${var.project}/${var.environment}/openvpn_sg_id"
 }
--- 98-openvpn/main.tf
+++ 98-openvpn/main.tf
@@ -1,17 +1,14 @@
-resource "aws_instance" "openvpnpractice" {
+resource "aws_instance" "openvpn" {
   ami           = local.ami_id
   instance_type = "t3.small"
   subnet_id = local.public_subnet_id
   vpc_security_group_ids = [local.openvpn_sg_id]
-   
-  user_data = file("vpn.sh") # we are using user_data to run the bootstrap script to update the /etc/hosts file in all the instances with the private IP address of the database instance. this is required because we are using private IP address to connect to the database instance from other instances. so whenever we launch a new instance then we need to update the /etc/hosts file in all the instances with the private IP address of the database instance. this is required because we are using private IP address to connect to the database instance from other instances. so whenever we launch a new instance then we need to update the /etc/hosts file in all the instances with the private IP address of the database instance.
+  user_data = file("vpn.sh")
 
-  
-  tags = merge (
-    
-
+  tags = merge(
     {
-      Name = "${var.project}-${var.environment}-openvpn"
-    },local.common_tags
+        Name = "${var.project}-${var.environment}-openvpn"
+    },
+    local.common_tags
   )
 }
