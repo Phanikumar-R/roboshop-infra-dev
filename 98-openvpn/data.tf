3c8b09203ac6579985facea046ccf2a3280413ff
phani <vsphanikumar777@gmail.com>
2026-03-30 23:34:34 +0530

    terraform infra

--- 98-openvpn/data.tf
+++ 98-openvpn/data.tf
@@ -1,27 +1,30 @@
+# Fetch latest Amazon Linux 2 AMI (Free Tier compatible)
 data "aws_ami" "openvpn" {
-  most_recent      = true
-  owners           = ["679593333241"]
+  most_recent = true
+  owners      = ["137112412989"] # Amazon Linux owner
 
   filter {
     name   = "name"
-    values = ["OpenVPN Access Server Community Image-fe8020db-*"]
-  }
-
-  filter {
-    name   = "root-device-type"
-    values = ["ebs"]
+    values = ["amzn2-ami-hvm-2.0.*-x86_64-gp2"]
   }
 
   filter {
     name   = "virtualization-type"
     values = ["hvm"]
   }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
 }
 
+# Fetch public subnet IDs from SSM
 data "aws_ssm_parameter" "public_subnet_ids" {
-    name = "/${var.project}/${var.environment}/public_subnet_ids"
+  name = "/${var.project}/${var.environment}/public_subnet_ids"
 }
 
+# Fetch OpenVPN security group ID from SSM
 data "aws_ssm_parameter" "openvpn_sg_id" {
-    name = "/${var.project}/${var.environment}/openvpn_sg_id"
+  name = "/${var.project}/${var.environment}/openvpn_sg_id"
 }
