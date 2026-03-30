4b6c7c11f06b64702a48a58f4e8a649a15607ad1
phani <vsphanikumar777@gmail.com>
2026-03-30 22:07:27 +0530

    terraform infra

--- 95-cdn/main.tf
+++ 95-cdn/main.tf
@@ -75,7 +75,7 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
   )
 
   viewer_certificate {
-    acm_certificate_arn = data.aws_acm_certificate.my_domain.arn
+    acm_certificate_arn = local.acm_certificate_arn
     ssl_support_method  = "sni-only"
   }
 }
